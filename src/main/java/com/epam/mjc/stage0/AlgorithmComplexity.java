218d9ee00cd68152f2d90af4ffa24e6396a5825d
OlzhasAlseitov <olzhas010111@gmail.com>
2023-12-17 18:13:39 +0600

    Stage 0 - Module 5 - Final Task - 4

--- src/main/java/com/epam/mjc/stage0/AlgorithmComplexity.java
+++ src/main/java/com/epam/mjc/stage0/AlgorithmComplexity.java
@@ -20,7 +20,7 @@ public class AlgorithmComplexity {
      * Return the linear search complexity if the element you are looking for is at the end of the array.
      */
     public Complexity badLinearSearch() {
-        return Complexity.O_N;
+        return Complexity.O_N_2;
     }
 
     /**
@@ -41,6 +41,6 @@ public class AlgorithmComplexity {
      * Return the complexity of the twoCycleSorting algorithm.
      */
     public Complexity twoCycleSorting() {
-        return Complexity.O_N_2;
+        return Complexity.O_N;
     }
 }
